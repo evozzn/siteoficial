aa7522c1a31eb2fa772cd5a75894d44eea005ecd
RamonGSousa <ramonsousa.designer@gmail.com>
2025-12-19 13:51:50 -0300

    Remover seta indicadora de scroll da pÃ¡gina inicial

--- app/page.tsx
+++ app/page.tsx
@@ -92,23 +92,6 @@ export default function Home() {
               Saiba Mais
             </Link>
           </div>
-
-          {/* Indicador de scroll (opcional) */}
-          <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-            <svg
-              className="w-6 h-6 text-primary"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M19 14l-7 7m0 0l-7-7m7 7V3"
-              />
-            </svg>
-          </div>
         </div>
       </div>
     </div>
